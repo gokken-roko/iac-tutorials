063c08b876d97ad92424e4176f8b342d54a2120a
gokken-roko <kenta_gotou@kadokawa.jp>
2021-10-27 18:13:50 +0900

    Deploy argoCD

--- kubernetes-iac-testbed/README.md
+++ kubernetes-iac-testbed/README.md
@@ -1,3 +1,39 @@
 # kubernetes-iac-testbed
 
-wip
+- [pulumi automation api](https://www.pulumi.com/docs/guides/automation-api)
+
+## Pre-requisites
+
+kubernetes クラスタの準備 または az コマンドの準備
+
+```
+$ yarn
+```
+
+## pulumi
+
+Install Pulumi CLI
+
+https://www.pulumi.com/docs/reference/cli/
+
+```console
+$ export PULUMI_CONFIG_PASSPHRASE=password
+```
+
+## Run
+
+```console
+$ kubectl config use-context $YOUR_CONTEXT
+$ yarn start
+```
+
+```console
+$ kubectl port-forward services/argo-cd-argocd-server 8080:80 -n=pulumi-argo
+$ open https://localhost:8080
+```
+
+## Cleanup
+
+```console
+$ yarn start destroy
+```
---
+++ kubernetes-iac-testbed/src/argo.ts
@@ -0,0 +1,46 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as k8s from "@pulumi/kubernetes";
+
+interface argoProps {
+  namespace?: string;
+}
+
+export class Argo extends pulumi.ComponentResource {
+  public helmUrn: pulumi.Output<string>;
+
+  constructor(
+    name: string,
+    props: argoProps,
+    opts?: pulumi.ComponentResourceOptions
+  ) {
+    super("kubernetes:helm:argo", name, {}, opts);
+
+    const n = props.namespace || "argo";
+    const argoNamaspace = new k8s.core.v1.Namespace(
+      n,
+      {
+        metadata: { name: n },
+      },
+      opts
+    );
+
+    const argocd = new k8s.helm.v3.Chart("argo-cd", {
+      chart: "argo-cd",
+      version: "3.26.2",
+      namespace: argoNamaspace.metadata.name,
+      fetchOpts: {
+          repo: "https://argoproj.github.io/argo-helm"
+      },
+      values: {
+        server: {
+          config: {
+            url: "http://localhost:8080"
+          },
+        },
+      },
+    });
+
+    this.helmUrn = argocd.urn;
+    this.registerOutputs();
+  }
+}
