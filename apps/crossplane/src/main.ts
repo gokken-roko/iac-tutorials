685c6a1b0119c85f4dae9266e46441135d25d724
gokken-roko <kenta_gotou@kadokawa.jp>
2022-08-24 20:14:26 +0900

    Update command

--- apps/crossplane/src/main.ts
+++ apps/crossplane/src/main.ts
@@ -1,6 +1,19 @@
-import { Preview } from "./automation";
+import { Preview, Deploy } from "./automation";
 
-Preview().catch((error) => {
-  console.log(error);
-  process.exit(1);
-});
+const args = process.argv.slice(2)
+console.log("Option: ", args[0])
+
+switch (args[0]) {
+  case "preview":
+    console.log("Preview...")
+    Preview().catch((error) => {
+      console.log(error);
+      process.exit(1);
+    });
+    break;
+  default:
+    Deploy().catch((error) => {
+      console.log(error);
+      process.exit(1);
+    });
+}
